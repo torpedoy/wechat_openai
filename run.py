7422b9e93c1fcc67c79272bdf05fbf83f6f0d798
虞雷 <624865374qq.com@gmail.com>
2023-03-07 18:44:15 +0800

    connect to WeChat

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-from flask import Flask
+import hashlib
+from os import abort
+
+from flask import Flask, request
 from flask_cors import CORS
 from werobot.contrib.flask import make_view
 
-from wechat import message
+from wechat.message import robot
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
@@ -10,15 +13,9 @@ CORS(app, resources=r'/*')
 app.add_url_rule(
     rule='/wechat',
     endpoint='werobot',
-    view_func=make_view(message),
+    view_func=make_view(robot),
     methods=['GET', 'POST']
 )
-
-
-@app.route("/", methods=['GET', 'POST'])
-def base():
-    return "hello"
-
 
 if __name__ == '__main__':
     app.run(
--- wechat/message.py
+++ wechat/message.py
@@ -1,16 +1,22 @@
+import hashlib
+from os import abort
+
+from flask import request
 from werobot import WeRoBot
 from werobot.replies import WeChatReply, TextReply, ImageReply
 
 robot = WeRoBot(
-    token="ddd"
+    token="dddd"
 )
-
+robot.config["APP_ID"] = "$$$$$$"               # 写入开发者ID
+robot.config["ENCODING_AES_KEY"] = "$$$$$$"
 
 @robot.subscribe
 def subscribe(message):
     return "你好，欢迎关注"
 
-
 @robot.text
 def text(message):
-    return "xxx"
+    openid = message.FromUserName
+
+    return "生成中。。。"
